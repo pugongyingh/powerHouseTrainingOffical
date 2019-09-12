bbf4b3a60a8099a4e2bf72addd66ca192318f68c
harshit77 <hsahu77@gmail.com>
2019-09-13 00:56:50 +0530

    Update and rename utlis.js to Authentication.js

--- src/Authentication.js
+++ src/Authentication.js
@@ -2,7 +2,7 @@ const jwt= require('jsonwebtoken');
 const APP_SECRET="Powerhouse_gym_training_yard";
 
 function getUserId(context) {
-    const auth=context.request.headers.authorization;
+    const auth=context.headers.authorization;
     console.log(auth);
     if(auth) {
         const token = auth.replace('Bearer ','');
