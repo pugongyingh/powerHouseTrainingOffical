fc5db03293d37e28a5a5e429a06e67175a2b58f4
harshit77 <hsahu77@gmail.com>
2019-09-13 01:03:55 +0530

    Update Authentication.js

--- src/Authentication.js
+++ src/Authentication.js
@@ -2,8 +2,7 @@ const jwt= require('jsonwebtoken');
 const APP_SECRET="Powerhouse_gym_training_yard";
 
 function getUserId(context) {
-    const auth=context.headers.authorization;
-    console.log(auth);
+    const auth=context.event.headers.authorization;
     if(auth) {
         const token = auth.replace('Bearer ','');
         const {userId}=jwt.verify(token,APP_SECRET);
